bde9ff8a707113c215bccf02b681ebe9283c8071
bl15343 <bl15343@gmail.com>
2019-07-13 10:12:45 -0400

    Additional advanced exercises

---
+++ advanced/decorators.py
@@ -0,0 +1,54 @@
+from time import sleep
+from functools import wraps
+import logging
+logging.basicConfig()
+log = logging.getLogger("retry")
+
+def retry(f):
+    @wraps(f)
+    def wrapper_function(*args, **kwargs):
+
+        MAX_ATTEMPTS = 5
+
+        for attempt in range(1, MAX_ATTEMPTS + 1):
+
+            try:
+
+                return f(*args, **kwargs)
+            except Exception:
+
+                log.exception(
+                    "Attempt %s/%s failed: %s",
+                    attempt,
+                    MAX_ATTEMPTS,
+                    (args, kwargs)
+                )
+
+                sleep(10*attempt)
+
+        log.critical(
+            "All %s attempts failed: %s",
+            MAX_ATTEMPTS,
+            (args, kwargs)
+        )
+
+    return wrapper_function
+
+counter = 0
+
+@retry
+def save_to_database(arg):
+    print("Write to a database or make a network call")
+    print("This will automatically retry if an exception is encountered")
+
+    global counter
+    counter += 1
+
+    #This will throw an exception in the first call
+    #And will work fine in the second call (i.e. a retry)
+
+    if counter < 2:
+        raise ValueError(arg)
+
+if __name__ == '__main__':
+    save_to_database('Some bad value')
---
+++ advanced/list_comprehensions.py
@@ -0,0 +1,5 @@
+listone = [2, 3, 4]
+#Initialize the second list iff i > 2, and set the values to twice
+#the value in listone
+listtwo = [2*i for i in listone if i > 2]
+print(listtwo)
